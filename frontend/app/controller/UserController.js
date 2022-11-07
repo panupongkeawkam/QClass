d084c88ba803eed769d12d2ba5481829949a48f2
pithawat565 <pithawat2624@gmail.com>
2022-11-07 16:22:13 +0700

    Add new file (hello) and comment userController

--- frontend/app/controller/UserController.js
+++ frontend/app/controller/UserController.js
@@ -1,39 +1,39 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
+// import AsyncStorage from "@react-native-async-storage/async-storage";
 
-import axios from "axios";
-import config from "../assets/api-config";
+// import axios from "axios";
+// import config from "../assets/api-config";
 
-function formatDate(dateFormat) {
-  var dateTime = new Date(dateFormat);
-  var date = `${dateTime.getFullYear()}-${(dateTime.getMonth() + 1 + 1000)
-    .toString()
-    .slice(2)}-${(dateTime.getDate() + 1000).toString().slice(2)}`;
-  var time = `${(dateTime.getHours() + 1000).toString().slice(2)}:${(
-    dateTime.getMinutes() + 1000
-  )
-    .toString()
-    .slice(2)}:${(dateTime.getSeconds() + 1000).toString().slice(2)}`;
-  dateFormat = `${date} ${time}`;
-  return dateFormat;
-}
+// function formatDate(dateFormat) {
+//   var dateTime = new Date(dateFormat);
+//   var date = `${dateTime.getFullYear()}-${(dateTime.getMonth() + 1 + 1000)
+//     .toString()
+//     .slice(2)}-${(dateTime.getDate() + 1000).toString().slice(2)}`;
+//   var time = `${(dateTime.getHours() + 1000).toString().slice(2)}:${(
+//     dateTime.getMinutes() + 1000
+//   )
+//     .toString()
+//     .slice(2)}:${(dateTime.getSeconds() + 1000).toString().slice(2)}`;
+//   dateFormat = `${date} ${time}`;
+//   return dateFormat;
+// }
 
-// Use for initial project
-const userInitialize = async () => {
-  let user = JSON.parse(await AsyncStorage.getItem("user"));
-  var isNoUser = !user;
-  if (isNoUser) {
-    user = await axios
-      .post(`http://${config.ip}:3000/user`)
-      .then((res) => {
-        return res.data;
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-    user.registerDatetime = formatDate(user.registerDatetime);
-    await AsyncStorage.setItem("user", JSON.stringify(user));
-  }
-  return user;
-};
+// // Use for initial project
+// const userInitialize = async () => {
+//   let user = JSON.parse(await AsyncStorage.getItem("user"));
+//   var isNoUser = !user;
+//   if (isNoUser) {
+//     user = await axios
+//       .post(`http://${config.ip}:3000/user`)
+//       .then((res) => {
+//         return res.data;
+//       })
+//       .catch((err) => {
+//         console.error(err);
+//       });
+//     user.registerDatetime = formatDate(user.registerDatetime);
+//     await AsyncStorage.setItem("user", JSON.stringify(user));
+//   }
+//   return user;
+// };
 
-export { userInitialize };
+// export { userInitialize };
