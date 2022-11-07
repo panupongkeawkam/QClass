3a366eb15fb409873d8aceaf1d4485e62ec27e43
Panupong Keawkam <63070132@kmitl.ac.th>
2022-11-07 16:00:28 +0700

    Update announcement

--- frontend/app/controller/RoomController.js
+++ frontend/app/controller/RoomController.js
@@ -1,20 +1,46 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import axios from "axios";
-import config from "../assets/api-config"
+import config from "../assets/api-config";
 
 function formatDate(dateFormat) {
   var dateTime = new Date(dateFormat);
-  var date = `${dateTime.getFullYear()}-${(dateTime.getMonth() + 1 + 1000)
-    .toString()
-    .slice(2)}-${(dateTime.getDate() + 1000).toString().slice(2)}`;
+  var now = new Date();
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  var date = `${(dateTime.getDate() + 1000).toString().slice(2)} ${
+    monthNames[dateTime.getMonth()]
+  }`;
   var time = `${(dateTime.getHours() + 1000).toString().slice(2)}:${(
     dateTime.getMinutes() + 1000
   )
     .toString()
-    .slice(2)}:${(dateTime.getSeconds() + 1000).toString().slice(2)}`;
-  dateFormat = `${date} ${time}`;
-  return dateFormat;
+    .slice(2)}`;
+
+  var isToday = now.getDate() - dateTime.getDate() === 0;
+  var isYesterday = now.getDate() - dateTime.getDate() === 1;
+  var isSameMonth = now.getMonth() - dateTime.getMonth() === 0;
+  var isSameYear = now.getYear() - dateTime.getYear() === 0;
+
+  if (isToday && isSameMonth && isSameYear) {
+    return time;
+  } else if (isYesterday && isSameMonth && isSameYear) {
+    return `Yesterday ${time}`;
+  }
+
+  return `${date} ${time}`;
 }
 
 function inviteCodeGenerator() {
@@ -31,7 +57,7 @@ function inviteCodeGenerator() {
 const createNewRoom = async (userId, title, iconName) => {
   //Variables
   var inviteCode = inviteCodeGenerator();
-  title = title.trim()
+  title = title.trim();
   var newRoom = await axios
     .post(`http://${config.ip}:3000/user/${userId}/room`, {
       room: {
@@ -49,21 +75,23 @@ const createNewRoom = async (userId, title, iconName) => {
     });
   return newRoom;
 };
+
 const leaveRoom = async (userId, room) => {
   var leave = await axios
-  .put(`http://${config.ip}:3000/participant/${userId}`, {
-    room: room
-  }).then((res) => {
-    console.log(res.data);
-    return res.data
-  })
-  .catch((err) => {
-    throw {message : err.response.data}
-  });
-  return leave
-}
+    .put(`http://${config.ip}:3000/participant/${userId}`, {
+      room: room,
+    })
+    .then((res) => {
+      console.log("Controller :  , when leave room is success : ", res.data);
+      return res.data;
+    })
+    .catch((err) => {
+      throw { message: err.response.data };
+    });
+  return leave;
+};
+
 const joinRoom = async (userId, inviteCode) => {
-  console.log("Controller : ", userId)
   inviteCode = inviteCode.trim();
 
   var isNullInviteCode = inviteCode.length <= 0;
@@ -78,8 +106,58 @@ const joinRoom = async (userId, inviteCode) => {
     })
     .then((res) => res.data)
     .catch((err) => {
-      throw {message : err.response.data}
+      throw { message: err.response.data };
     });
 };
 
-export { createNewRoom, joinRoom, leaveRoom };
+const fetchAnnouncements = async (roomId) => {
+  var announcements = await axios
+    .get(`http://${config.ip}:3000/room/${roomId}/announcement`)
+    .then((res) => {
+      return res.data;
+    })
+    .catch((err) => {
+      throw { message: err.response.data };
+    });
+  announcements = announcements.announcements.map((announcement) => {
+    var time = formatDate(announcement.createDatetime);
+    return {
+      time: time,
+      message: announcement.message,
+    };
+  });
+  return announcements;
+};
+
+const sendAnnouncement = async (userId, roomId, message) => {
+  message = message.trim();
+
+  var isMessageNull = message.length <= 0;
+
+  if (isMessageNull) {
+    throw { message: "Message cannot be empty`" };
+  }
+
+  var announcement = await axios
+    .post(`http://${config.ip}:3000/room/${roomId}/announcement`, {
+      userId: userId,
+      message: message,
+    })
+    .then((res) => res.data)
+    .catch((err) => {
+      throw { message: err.response.data };
+    });
+  var formatDateTime = formatDate(announcement.announcement.time);
+  return {
+    message: message,
+    time: formatDateTime,
+  };
+};
+
+export {
+  createNewRoom,
+  joinRoom,
+  leaveRoom,
+  sendAnnouncement,
+  fetchAnnouncements,
+};
